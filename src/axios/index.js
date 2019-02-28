10f158d6138e8a88224d978de4af28a98c4c21ec
Portal <zhaoyoutianzyt@163.com>
2019-02-28 20:37:39 +0800

    权限设置

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,4 +1,6 @@
 import JsonP from 'jsonp'
+import axios from 'axios'
+import { Modal } from 'antd'
 export default class Axios {
     static jsonp(options) {
         return new Promise((res, rej) => {
@@ -8,5 +10,24 @@ export default class Axios {
                 response.status == 'success' ? res(response) : rej(response.message)
             })
         })
+    }
+
+    static ajax(options) {
+        let baseApi = 'https://www.easy-mock.com/mock/5a7278e28d0c633b9c4adbd7/api';
+        return new Promise((resolve, reject) => {
+            axios({
+                url: options.url,
+                method: 'get',
+                baseURL: baseApi,
+                timeout: 5000,
+                params: (options.data && options.data.params) || ''
+            }).then((response) => {
+                if (response.status == '200') {
+                    resolve(response.data);
+                } else {
+                    reject(response.data);
+                }
+            })
+        });
     }
 }
--- src/pages/permissions/index.js
+++ src/pages/permissions/index.js
@@ -1,17 +1,81 @@
 import React from 'react';
-import { Table, Card } from 'antd';
+import { Table, Card, Button } from 'antd';
+import Utils from './../../utils/utils';
+import ETable from './../../components/ETable';
+import axios from './../../axios'
 export default class Permission extends React.Component {
     constructor(props) {
         super(props)
     }
+    state = {}
+    componentWillMount() {
+        this.requestList();
+    }
+    requestList = () => {
+        axios.ajax({
+            url: '/role/list',
+            data: {
+                params: {}
+            }
+        }).then((res) => {
+            console.log(res);
+            if (res.code == 0) {
+                let list = res.result.item_list.map((item, i) => {
+                    item.key = i;
+                    return item;
+                })
+                this.setState({
+                    list
+                })
+            }
+        })
+    }
     render() {
-        const columns = {
-            title: 'id',
-            dataIndex: 'id1'
-        }
+        const columns = [{
+            title: '角色ID',
+            dataIndex: 'id'
+        }, {
+            title: '角色名称',
+            dataIndex: 'role_name'
+        }, {
+            title: '创建时间',
+            dataIndex: 'create_time',
+            render: Utils.formateDate
+        }, {
+            title: '使用状态',
+            dataIndex: 'status',
+            render(status) {
+                return status == 1 ? '启用' : '停用'
+            }
+        }, {
+            title: '授权时间',
+            dataIndex: 'authorize_time',
+            render: Utils.formateDate
+        }, {
+            title: '授权人',
+            dataIndex: 'authorize_user_name'
+        }]
         return (
             <div>
-                jdhsgfhdgfgsdf
+                <Card>
+                    <Button type="primary"
+                        style={{ margin: '0 10px' }}
+                        onClick={this.handleRole}>创建角色</Button>
+                    <Button type="primary"
+                        style={{ margin: '0 10px' }}
+                        onClick={this.handlePermission}>设置权限</Button>
+                    <Button type="primary"
+                        style={{ margin: '0 10px' }}
+                        onClick={this.handleUserAuth}>用户授权</Button>
+                </Card>
+                <div className="content-wrap">
+                    <ETable
+                        updateSelectedItem={Utils.updateSelectedItem.bind(this)}
+                        selectedRowKeys={this.state.selectedRowKeys}
+                        dataSource={this.state.list}
+                        columns={columns}
+                    ></ETable>
+                </div>
             </div>
         )
     }
